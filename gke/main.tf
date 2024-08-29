795af45136366b3fc0bd89b3f5bc4d600f494385
raziqtechnologies <81863529+raziqtechnologies@users.noreply.github.com>
2024-08-29 16:54:40 +0100

    roles updated

--- gke/main.tf
+++ gke/main.tf
@@ -47,7 +47,7 @@ resource "google_container_cluster" "primary" {
   depends_on = [google_project_service.enable_kubernetes_api]	
   name     = var.cluster_name
   location = var.region
-  deletion_protection = false #Not for production
+  deletion_protection = false
   initial_node_count = 1
 
   node_config {
@@ -66,7 +66,7 @@ resource "google_container_node_pool" "primary_nodes" {
   cluster    = google_container_cluster.primary.name
   location   = google_container_cluster.primary.location
   node_count = var.node_count
-  deletion_protection = false #Not for production
+
 
   node_config {
     machine_type = var.machine_type
