a27a838ee6c3e16b9331d4dbcaa224f0d6cd4f48
raziqtechnologies <81863529+raziqtechnologies@users.noreply.github.com>
2024-09-09 21:48:08 +0100

    aws added

---
+++ aws/main.tf
@@ -0,0 +1,92 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-pega"
+    key    = "terraform/eks/state"
+    region = var.region
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+# Enable the necessary IAM roles and policies for EKS
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_policy" {
+  role       = aws_iam_role.eks_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# Service Account for Terraform
+resource "aws_iam_role" "terraform_sa" {
+  name = "terraform-sa"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "terraform_sa_policy" {
+  role       = aws_iam_role.terraform_sa.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "primary" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_policy
+  ]
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "primary_nodes" {
+  cluster_name    = aws_eks_cluster.primary.name
+  node_group_name = "primary-node-group"
+  node_role_arn   = aws_iam_role.terraform_sa.arn
+  subnet_ids      = var.subnet_ids
+  instance_types  = [var.instance_type]
+
+  scaling_config {
+    desired_size = var.node_count
+    max_size     = var.node_count
+    min_size     = 1
+  }
+}
+
+# Optional: Allow Terraform Service Account to manage EKS
+resource "aws_iam_role_policy_attachment" "eks_admin_policy" {
+  role       = aws_iam_role.terraform_sa.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+}
---
+++ aws/variables.tf
@@ -0,0 +1,24 @@
+variable "project_id" {
+  description = "The project ID where the GKE cluster will be created"
+  default     = "pegajenkins"
+}
+
+variable "region" {
+  description = "The region where the GKE cluster will be created"
+  default     = "eu-west-2"
+}
+
+variable "cluster_name" {
+  description = "The name of the GKE cluster"
+  default     = "pega-gcp-cluster"
+}
+
+variable "machine_type" {
+  description = "The machine type to use for nodes"
+  default     = "t3.medium"
+}
+
+variable "node_count" {
+  description = "The number of nodes in the node pool"
+  default     = 1
+}
