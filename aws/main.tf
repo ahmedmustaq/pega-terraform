6e29695aebd6330504218b46b7921ff8647d8e0e
raziqtechnologies <81863529+raziqtechnologies@users.noreply.github.com>
2024-09-12 12:24:28 +0100

    auto assign

--- aws/main.tf
+++ aws/main.tf
@@ -24,6 +24,7 @@ resource "aws_subnet" "public_subnet_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-west-2a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-subnet-1"
@@ -34,6 +35,7 @@ resource "aws_subnet" "public_subnet_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-west-2b"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-subnet-2"
